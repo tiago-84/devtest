b1ca21789995073c702f970572409ddac5c25384
tiago <sawadeekrap1@gmail.com>
2023-05-22 00:50:35 -0300

    teste

---
+++ resources/views/form.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="{{}}" method="POST">
+        <label for="">Nome</label> <br />
+        <input type="text"> <br />
+        <label for="">Custo</label> <br />
+        <input type="text"> <br />
+        <label for="">Preco</label> <br />
+        <input type="text"> <br />
+        <label for="">Quantidade</label> <br />
+        <input type="text"> <br />
+        <button>Salvar</button>
+    </form>
+</body>
+</html>
